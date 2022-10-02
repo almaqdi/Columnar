49ec4fa6386632b276143aedd7cbce590f4d60c5
Abdulrahman Almaqdi <Almaqdi@DESKTOP-VHN80PL>
2022-10-02 14:18:33 +0300

    This is Columnar Encription And Decription

---
+++ Columnar/Columnar.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Columnar
+{
+    partial class Columnar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Encript";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Plane text:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Cipher text:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(279, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Decipher text:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 179);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(279, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Decript";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 314);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Columnar/Columnar.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Columnar
+{
+    public partial class Columnar : Form
+    {
+        public Columnar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int key = textBox2.Text.Length;
+            string plane = textBox1.Text.ToUpper();
+            List<List<string>> list = new List<List<string>>();
+            for (int k = 0; k < key; k++)
+            {
+                list.Add(new List<string>());
+                list[k].Add("");
+            }
+            int num = 0;
+            for (int i = 0; i < plane.Length; i++)
+            {
+                if(num == key)
+                {
+                    num = 0;
+                }
+                list[num][0] += plane[i];
+                num++;
+            }
+
+            List<char> ke = textBox2.Text.ToUpper().ToList();
+            ke.Sort();
+            for (int m = 0; m < key; m++)
+            {
+                int index = textBox2.Text.ToUpper().IndexOf(ke[m]);
+                textBox3.Text += list[index][0];
+            }
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int col = textBox3.Text.Length / textBox2.Text.Length;
+
+            int still = (textBox3.Text.Length % textBox2.Text.Length);
+
+            string cipher = textBox3.Text;
+            List<char> sorted = new List<char>();
+            List<string> items = new List<string>();
+            for (int i = 0; i < textBox2.Text.Length; i++)
+            {
+                items.Add("");
+                sorted.Add(textBox2.Text[i]);
+            }
+            sorted.Sort();
+            int check = still;
+            for (int c = 0; c < textBox2.Text.Length; c++)
+            {
+                int index = textBox2.Text.IndexOf(sorted[c]);
+
+                items[index] += cipher.Substring(0, col);
+                cipher = cipher.Remove(0, col);
+                if (still > 0 && check > index)
+                {
+                    items[index] += cipher.Substring(0, 1);
+                    cipher = cipher.Remove(0, 1);
+                    still--;
+                }
+            }
+            
+            for (int n = 0; n < col+1; n++)
+            {
+                for (int l = 0; l < textBox2.Text.Length; l++)
+                {
+                    try
+                    {
+                        textBox4.Text += items[l][n];
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            //int key = textBox2.Text.Length;
+            //int cilen = textBox3.Text.Length;
+            //int emp = 0;
+            //while(cilen%key != 0)
+            //{
+            //    cilen++;
+            //    emp++;
+            //}
+            //int col_num = cilen / key;
+            //List<string> items = new List<string>();
+
+
+        }
+    }
+}
